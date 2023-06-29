be005e902d57a9e824266ba6d3217609c375527d
weeger <romain.weeger@gmail.com>
2023-06-29 11:55:43 +0200

    First commit

---
+++ src/DependencyInjection/WexampleSymfonyDataSyncExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Wexample\SymfonyDataSync\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Wexample\SymfonyHelpers\DependencyInjection\AbstractWexampleSymfonyExtension;
+
+class WexampleSymfonyDataSyncExtension extends AbstractWexampleSymfonyExtension
+{
+    public function load(
+        array $configs,
+        ContainerBuilder $container
+    ) {
+        $this->loadConfig(
+            __DIR__,
+            $container
+        );
+    }
+}
---
+++ src/WexampleSymfonyDataSyncBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wexample\SymfonyDataSync;
+
+use Wexample\SymfonyHelpers\AbstractBundle;
+
+class WexampleSymfonyDataSyncBundle extends AbstractBundle
+{
+}
